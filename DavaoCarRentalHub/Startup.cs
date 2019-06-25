4db91f0bcc932c3ee40945acb1a22184b869a09e
Jahdiel Villosa <jahdielsvillosa@gmail.com>
2019-06-25 15:15:55 +0800

    Initial Setup

---
+++ DavaoCarRentalHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DavaoCarRentalHub.Startup))]
+namespace DavaoCarRentalHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
